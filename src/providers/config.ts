684dd0e9e52a99bb3c4e5a034152a6babc51c3a5
kolawole <kolawolegolulana@gmail.com>
2017-12-11 21:12:31 +0200

    added https

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Config {
-	static SERVER_URL = "http://ssl-real-estate-api.herokuapp.com/api/";
+	static SERVER_URL = "https://ssl-real-estate-api.herokuapp.com/api/";
 }
